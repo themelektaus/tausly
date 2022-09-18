94f8f3a7beda0288a86d5e2a2ba6c266283b5067
MelekTaus Moribundus <melektaus@outlook.at>
2022-09-18 20:13:10 +0200

    Extend RESET instruction

--- classes/line/54-reset.js
+++ classes/line/54-reset.js
@@ -4,14 +4,54 @@ class ResetLine extends Line
     
     static parse(options)
     {
-        if (options.code.matchKeyword("RESET"))
+        let matches
+        
+        matches = options.code.matchKeyword("RESET", 1)
+        if (matches)
+        {
+            const line = new ResetLine(options)
+            line.dimName = matches[1]
+            return line
+        }
+        
+        matches = options.code.matchKeyword("RESET")
+        if (matches)
+        {
             return new ResetLine(options)
+        }
         
         return null
     }
     
     * step()
     {
+        if (this.dimName)
+        {
+            let dim = this.parent.get(this.dimName)
+            if (dim !== undefined)
+            {
+                if (dim.length)
+                {
+                    for (const i in dim)
+                    {
+                        if (dim[i].length)
+                        {
+                            for (const j in dim[i])
+                                dim[i][j] = 0
+                        }
+                        else
+                            dim[i] = 0
+                    }
+                }
+                else
+                {
+                    dim = 0
+                }
+                this.parent.set(this.dimName, dim)
+            }
+            return
+        }
+        
         const ctx = this.root.getContext()
         
         const transforms = this.root.getHistory("TRANSFORMS")[0]
